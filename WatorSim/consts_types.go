447a55313594188d18e732981d93967635701eb9
Veneta <venkireva80@gmail.com>
2023-07-19 19:00:20 +0300

    Add consts and types

---
+++ WatorSim/consts_types.go
@@ -0,0 +1,25 @@
+package WatorSim
+
+const (
+	NORTH = iota
+	SOUTH
+	EAST
+	WEST
+)
+
+const (
+	FISH = iota
+	SHARK
+)
+
+type Game struct{}
+
+type coordinate struct {
+	x, y int
+}
+
+type creature struct {
+	age     int
+	health  int
+	species int
+}
