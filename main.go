e36d6248217439e46b0fa004d65c547dadeda580
Veneta <venkireva80@gmail.com>
2023-08-05 17:22:50 +0300

    Add max chronon parameter and run function

--- main.go
+++ main.go
@@ -6,18 +6,20 @@ import (
 )
 
 func init() {
-	flag.IntVar(&WatorSim.InitFishCount, "fc", 10000, "Initial number of fish")
-	flag.IntVar(&WatorSim.InitSharksCount, "sc", 400, "Initial number of sharks")
-	flag.IntVar(&WatorSim.BreedFish, "fb", 50, "Length of fish breeding cycle")
-	flag.IntVar(&WatorSim.BreedSharks, "sb", 70, "Length of shark breeding cycle")
-	flag.IntVar(&WatorSim.Starve, "s", 150, "Chronons until starvation")
-	flag.IntVar(&WatorSim.Width, "w", 4000, "Width of the board")
-	flag.IntVar(&WatorSim.Height, "h", 4010, "Height of the board")
+	flag.IntVar(&WatorSim.InitFishCount, "fc", 5000, "Initial number of fish")
+	flag.IntVar(&WatorSim.InitSharksCount, "sc", 500, "Initial number of sharks")
+	flag.IntVar(&WatorSim.BreedFish, "fb", 20, "Length of fish breeding cycle")
+	flag.IntVar(&WatorSim.BreedSharks, "sb", 30, "Length of shark breeding cycle")
+	flag.IntVar(&WatorSim.Starve, "s", 50, "Chronons until starvation")
+	flag.IntVar(&WatorSim.Width, "w", 500, "Width of the board")
+	flag.IntVar(&WatorSim.Height, "h", 500, "Height of the board")
 	flag.IntVar(&WatorSim.ThreadCount, "t", 8, "Thread count")
+	flag.IntVar(&WatorSim.MaxChronon, "mch", 200, "Max chronons to run the simulation for")
 
 	flag.Parse()
 }
 
 func main() {
 	checkParams()
+	WatorSim.CreateAndRunCheckboard()
 }
