0ebe5185d9543895ea766368ac6416ba6b12728a
Veneta <venkireva80@gmail.com>
2023-08-06 10:00:29 +0300

    SetGOMAXPROCS

--- main.go
+++ main.go
@@ -4,21 +4,23 @@ import (
 	"Wa-Tor/WatorSim"
 	"flag"
 	"fmt"
+	"runtime"
 	"time"
 )
 
 func init() {
-	flag.IntVar(&WatorSim.InitFishCount, "fc", 5000, "Initial number of fish")
-	flag.IntVar(&WatorSim.InitSharksCount, "sc", 500, "Initial number of sharks")
+	flag.IntVar(&WatorSim.InitFishCount, "fc", 500000, "Initial number of fish")
+	flag.IntVar(&WatorSim.InitSharksCount, "sc", 50000, "Initial number of sharks")
 	flag.IntVar(&WatorSim.BreedFish, "fb", 20, "Length of fish breeding cycle")
 	flag.IntVar(&WatorSim.BreedSharks, "sb", 30, "Length of shark breeding cycle")
 	flag.IntVar(&WatorSim.Starve, "s", 50, "Chronons until starvation")
-	flag.IntVar(&WatorSim.Width, "w", 500, "Width of the board")
-	flag.IntVar(&WatorSim.Height, "h", 500, "Height of the board")
-	flag.IntVar(&WatorSim.ThreadCount, "t", 1, "Thread count")
-	flag.IntVar(&WatorSim.MaxChronon, "mch", 1000, "Max chronons to run the simulation for")
-
+	flag.IntVar(&WatorSim.Width, "w", 4000, "Width of the board")
+	flag.IntVar(&WatorSim.Height, "h", 4000, "Height of the board")
+	flag.IntVar(&WatorSim.ThreadCount, "t", 4, "Thread count")
+	flag.IntVar(&WatorSim.MaxChronon, "mch", 100, "Max chronons to run the simulation for")
 	flag.Parse()
+
+	runtime.GOMAXPROCS(runtime.NumCPU())
 }
 
 func main() {
